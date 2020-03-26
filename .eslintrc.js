7b91a1a65e59efc172abb353b4298310bd797e82
LaamGinghong <351390560@qq.com>
2020-03-26 16:35:58 +0800

    fix: eslint mixed ts and ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,28 +9,15 @@ module.exports = {
         'airbnb',
         'airbnb/hooks',
         'plugin:eslint-comments/recommended',
-        'plugin:import/typescript',
         'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
         'prettier',
         'prettier/react',
-        'prettier/@typescript-eslint',
     ],
     globals: {
         Atomics: 'readonly',
         SharedArrayBuffer: 'readonly',
     },
-    settings: {
-        'import/resolver': {
-            node: {
-                extensions: ['.ts', '.tsx', '.js', '.json'],
-            },
-            typescript: {
-                directory: [resolve('./tsconfig.json')],
-            },
-        },
-    },
-    parser: '@typescript-eslint/parser',
+    parser: 'babel-eslint',
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
@@ -40,19 +27,46 @@ module.exports = {
     },
     plugins: ['react', '@typescript-eslint'],
     rules: {
-        'import/extensions': [2, 'ignorePackages', { ts: 'never', tsx: 'never', json: 'never', js: 'never' }],
         'no-useless-constructor': 'off',
-        '@typescript-eslint/no-useless-constructor': 'error',
         'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-        'react/jsx-filename-extension': ['error', { extensions: ['.js', '.tsx'] }],
+        'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
         'react/state-in-constructor': [2, 'never'],
-        '@typescript-eslint/no-empty-interface': 'warn',
     },
     overrides: [
         {
             files: ['**/*.d.ts'],
             rules: {
                 'import/no-duplicate': 'off',
+            },
+        },
+        {
+            files: ['**/*.ts', '**/*.tsx'],
+            parser: '@typescript-eslint/parser',
+            extends: [
+                'airbnb',
+                'airbnb/hooks',
+                'plugin:eslint-comments/recommended',
+                'plugin:import/typescript',
+                'plugin:react/recommended',
+                'plugin:@typescript-eslint/recommended',
+                'prettier',
+                'prettier/react',
+                'prettier/@typescript-eslint',
+            ],
+            settings: {
+                'import/resolver': {
+                    typescript: {
+                        directory: [resolve('./tsconfig.json')],
+                    },
+                },
+            },
+            rules: {
+                'import/extensions': [2, 'ignorePackages', { ts: 'never', tsx: 'never', json: 'never', js: 'never' }],
+                'no-useless-constructor': 'off',
+                '@typescript-eslint/no-useless-constructor': 'error',
+                'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+                'react/state-in-constructor': [2, 'never'],
+                '@typescript-eslint/no-empty-interface': 'warn',
             },
         },
     ],
--- src/app/app.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from 'react'
-
-const App: FC = () => {
-    return <p>Hello world!</p>
-}
-
-export default App
